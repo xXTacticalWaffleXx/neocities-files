3c25e858c5f11b43ff0f6d37d3c735a62c0328d7
luna aphelion <luna@pop-os.localdomain>
2021-07-15 03:56:18 +0100

    added an upload script for linux-a

---
+++ html_2_server.sh
@@ -0,0 +1,29 @@
+cd bio
+neocities upload -d /bio index.html
+cd ..
+
+cd contact
+neocities upload -d /contact index.html
+cd ..
+
+cd cool
+neocities upload -d /cool index.html
+cd ..
+
+neocities upload index.html
+#no cd needed as this is in the root dir of the repo
+
+cd lol
+neocities upload -d /lol index.html
+cd ..
+
+neocities upload not_found.html
+#no cd needed as this is in the root dir of the repo
+
+cd rainmeter
+neocities upload -d /rainmeter index.html
+cd ..
+
+cd snom
+neocities upload -d /snom index.html
+cd..
