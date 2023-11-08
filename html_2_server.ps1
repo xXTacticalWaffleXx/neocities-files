fb896f704f531b193858695b77cca643fea835f8
Luna Aphelion <luna-aphelion@proton.me>
2023-11-08 06:33:31 +0000

    added quote

--- html_2_server.ps1
+++ html_2_server.ps1
@@ -4,6 +4,26 @@
 
 #please make sure you run this program in the root directory of your site otherwise you can cause havok
 
+#windows is fucking retarded so this script needs to be ran as admin (at least on my machine)
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+Write-Warning "Windows is fucking retarded, please run this script as admin :3"
+exit
+}
+else {
+Write-Host "script is elevated, continuing" -ForegroundColor Green
+}
+
+#make this thing actually work
+for ($var = 1; $var -le $args.length; $var++) {
+  switch ($args[$var]) {
+    c {git commit -m "added quote to /lol" -a}
+    p {git push}
+    Default {echo "fam what the fuck do you mean by that"
+    exit}
+  }
+}
+
 neocities upload -d /bio bio/index.html
 
 neocities upload -d /contact contact/index.html
